5dd7b49dc01fbbbd33a01b1a2db1ef37f72af934
exaltedny <nymarfo2024@gmail.com>
2026-05-14 01:26:39 +0000

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        else:
+            for chr in s:
+                if chr not in t:
+                    return False 
+                else:
+                    t = t.replace(chr,"", 1)
+            return True
